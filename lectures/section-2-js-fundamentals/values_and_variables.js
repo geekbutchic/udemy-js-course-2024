f02e2fe47492b589bf26f691044a09a68795636b
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-04-30 20:15:56 -0600

    creation of udemy js 2024 lecture files

---
+++ lectures/section-2-js-fundamentals/values_and_variables.js
@@ -0,0 +1,22 @@
+// The value in this example is 
+
+// Box explanation
+// Box is created/(variable) the label/(value) is amazing
+let js = 'amazing';
+
+// To use the value 
+console.log(js);
+
+// Values and Variables Assignment
+
+/*
+1. Declare variables called country, continent, and population and assign their values.
+
+2. Log their values to the console.
+*/
+
+let country = 'United States of America';
+let continent = 'North America';
+let population = 333300000;
+
+console.log(country, continent, population);
