7bb6ed835f73b7ab126679cf85417153618a3f8a
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-05-01 19:31:48 -0600

    adds lecture notes for lesson on data types in js

---
+++ lectures/section-2-js-fundamentals/data_types.js
@@ -0,0 +1,20 @@
+// 3 Most common Primitive Value Types
+
+//Number
+let age = 23;
+console.log(typeof age, age);
+
+//String
+let firstName = "Sonny";
+console.log(typeof firstName, firstName);
+
+//Boolean
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun, javascriptIsFun);
+
+//Changing a value in JS showing dynamic typing
+javascriptIsFun = "true";
+console.log(typeof javascriptIsFun, javascriptIsFun); //string true
+
+//To Note: when re-assigning a new variable you do not have to use let or const.  let or const would be an indication of creating a new variable.
+
