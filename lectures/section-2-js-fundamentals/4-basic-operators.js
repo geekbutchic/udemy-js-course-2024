f12bb8810f197a90d7ba38e038b153bddece8226
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-05-07 20:00:27 -0600

    adds notes for basic operators

---
+++ lectures/section-2-js-fundamentals/4-basic-operators.js
@@ -0,0 +1,55 @@
+// Basic Operators
+
+/*  
+What is an operator?
+
+* In JavaScript, operators are symbols or keywords that perform operations on one or more operands (values or variables) to produce a result. JavaScript supports various types of operators for different purposes, including arithmetic, assignment, comparison, logical, bitwise, and more. 
+*/
+
+const now = 2024;
+
+const ageSonny = now - 1984;
+const ageKelsey = now - 1998;
+console.log(ageSonny, ageKelsey); // 40
+
+
+/*  
+Arithmetic Operators 
+
+Addition ( + ) 
+Subtraction ( - )
+Multiplication ( * )
+Division ( / )
+Modulus ( % )
+Increment ( ++ )
+Decrement ( -- )
+*/
+
+let  addition = 5 + 5;
+console.log(addition); // 10
+
+let  subtraction = 10 - 2;
+console.log(subtraction); // 8
+
+let  multiplication = 10 * 10;
+console.log(multiplication); // 100
+
+let  division = 120 / 2;
+console.log(division); // 60
+
+let  modulus = 17 % 5;
+console.log(modulus); // 2
+
+let x = 5;        // Assign 5 to x
+let y = x++;      // Assign the value of x to y, then increment x by 1
+console.log(y);   // Output: 5 (y holds the original value of x)
+console.log(x);   // Output: 6 (x has been incremented)
+
+
+/* 
+Simple assignment ( = )
+Addition assignment ( +=) 
+Subtraction assignment ( -=)
+Multiplication assignment (*=)
+Division assignment (/=)
+*/
