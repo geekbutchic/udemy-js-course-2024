c8e3211b36aecb916a7fb901d5292795723b8fc4
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-05-13 16:56:40 -0600

    adds notes on basic operators

--- lectures/section-2-js-fundamentals/4-basic-operators.js
+++ lectures/section-2-js-fundamentals/4-basic-operators.js
@@ -12,7 +12,6 @@ const ageSonny = now - 1984;
 const ageKelsey = now - 1998;
 console.log(ageSonny, ageKelsey); // 40
 
-
 /*  
 Arithmetic Operators 
 
@@ -25,25 +24,25 @@ Increment ( ++ ) value = value + 1
 Decrement ( -- ) value = value - 1
 */
 
-let  addition = 5 + 5;
+let addition = 5 + 5;
 console.log(addition); // 10
 
-let  subtraction = 10 - 2;
+let subtraction = 10 - 2;
 console.log(subtraction); // 8
 
-let  multiplication = 10 * 10;
+let multiplication = 10 * 10;
 console.log(multiplication); // 100
 
-let  division = 120 / 2;
+let division = 120 / 2;
 console.log(division); // 60
 
-let  modulus = 17 % 5;
+let modulus = 17 % 5;
 console.log(modulus); // 2
 
-let x = 5;        // Assign 5 to x
-let y = x++;      // Assign the value of x to y, then increment x by 1
-console.log(y);   // Output: 5 (y holds the original value of x)
-console.log(x);   // Output: 6 (x has been incremented)
+let x = 5; // Assign 5 to x
+let y = x++; // Assign the value of x to y, then increment x by 1
+console.log(y); // Output: 5 (y holds the original value of x)
+console.log(x); // Output: 6 (x has been incremented)
 
 let z = 5;
 let w = z--;
@@ -71,7 +70,7 @@ Division assignment (/=)
 // Addition assignment
 let a = 5;
 a += 3;
-console.log(a); // 8 
+console.log(a); // 8
 
 // Subtraction assignment
 let b = 5;
@@ -79,7 +78,7 @@ b -= 3;
 console.log(b); // 2
 
 // Multiplication assignment
-let c = 5
+let c = 5;
 c *= 25;
 console.log(c); // 125
 
@@ -101,4 +100,4 @@ Greater than >=
 Less than <
 Less than <=
 
-*/
+*/ 
