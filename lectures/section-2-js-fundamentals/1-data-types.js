8ee787c40c5f7295974dc97ef577ff64fd51e9dc
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-06-10 20:09:09 -0600

    lecture notes on strings and template literals, and lecture 1 solution

--- lectures/section-2-js-fundamentals/1-data-types.js
+++ lectures/section-2-js-fundamentals/1-data-types.js
@@ -18,3 +18,22 @@ console.log(typeof javascriptIsFun, javascriptIsFun); //string true
 
 //To Note: when re-assigning a new variable you do not have to use let or const.  let or const would be an indication of creating a new variable.
 
+/*
+Practice Assignment :
+
+1: Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+2: Log their values to the console.
+*/
+
+//If you want to make the numbers more readable in your code, you can use underscores (_) as digit separators, which were introduced in ECMAScript 2021 (ES12). This does not affect the value of the number but makes it easier to read:
+
+const country = "United States of America";
+const continent = "North America";
+const population = 341_710_247;
+
+console.log(
+  `I live in the ${country} within ${continent} with an estimated population of ${population.toLocaleString(
+    "en-US"
+  )}`
+);
---
+++ lectures/section-2-js-fundamentals/6-strings_template_literals.js
@@ -0,0 +1,18 @@
+const firstName = "Sonny";
+const job = "student";
+const birthYear = 1984;
+const year = 2024;
+
+//Template Literal
+const sonny = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(sonny);
+
+/*
+When using multi-line strings press "return" and sue template literals.
+*/
+
+/*
+Practice assignment :
+
+
+*/
