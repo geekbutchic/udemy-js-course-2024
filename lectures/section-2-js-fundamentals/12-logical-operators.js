a4818ce845ac9a7e586f781b37191423b27846fc
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-07-23 16:37:15 -0600

    adds notes to logical operators lecture

--- lectures/section-2-js-fundamentals/12-logical-operators.js
+++ lectures/section-2-js-fundamentals/12-logical-operators.js
@@ -1,2 +1,22 @@
-'use strict'
+"use strict";
 
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision); // true
+console.log(hasDriversLicense || hasGoodVision); // true
+console.log(!hasDriversLicense); // true
+
+if (hasDriversLicense && hasGoodVision) {
+  console.log(`Sarah is able to drive!`);
+} else {
+  console.log(`Someone else should drive...`);
+}
+
+const isTired = false;
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log(`Sarah is able to drive!`);
+} else {
+  console.log(`Someone else should drive`);
+}
