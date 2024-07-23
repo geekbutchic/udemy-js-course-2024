e5812498a28e459fd0861fe4726b7854799d3107
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-07-23 14:44:39 -0600

    notes on logical operators

---
+++ lectures/section-2-js-fundamentals/11-boolean-logic.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Logical Operators
+
+/* 
+AND, OR, NOT
+
+Example : 
+A. Sarah has a driver's license
+B. Sarah has good vision
+*/
+
+// AND OPERATOR - TRUE when all or true
+
+// OR OPERATOR - TRUE when one are true
+
+// NOT OPERATOR - INVERTS true/false value (PRECEDENCE over AND, OR)
+
