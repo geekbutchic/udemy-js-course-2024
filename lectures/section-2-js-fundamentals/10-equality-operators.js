8311a6635b236e08bed83747f02d0b7c431748d0
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-07-23 15:19:22 -0600

    adds solution to lecture 10 challenge, also installs prompt library for use case

--- lectures/section-2-js-fundamentals/10-equality-operators.js
+++ lectures/section-2-js-fundamentals/10-equality-operators.js
@@ -16,8 +16,31 @@ if (age == 18) {
 
 // As a general rule avoid using loose equality operator.
 
-
 // Not equal operator strict version
 if (age !== 18) {
   console.log(`You are not an adult yet!`);
+}
+
+//================ Lecture Challenge ====================
+
+const prompt = require("prompt-sync")();
+
+// Example
+// const name = prompt("What is your name? ");
+// console.log(`Hello, ${name}!`);
+
+/*
+Lecture : Equality Operators : == vs ===
+*/
+
+const numNeighbors = Number(
+  prompt("How many neighbor countries does your country have? ")
+);
+
+if (numNeighbors === 1) {
+  console.log(`Only 1 border!`); // if 1
+} else if (numNeighbors > 1) {
+  console.log(`More than 1 border!`); // if 2
+} else {
+  console.log(`No borders!`); // if 0
 }
