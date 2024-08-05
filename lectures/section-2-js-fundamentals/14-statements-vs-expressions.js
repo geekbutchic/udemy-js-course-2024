75bfef77bef9cf414a68c41043535f18b5418909
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-08-05 16:57:47 -0600

    notes on statements vs expressions in js

---
+++ lectures/section-2-js-fundamentals/14-statements-vs-expressions.js
@@ -0,0 +1,21 @@
+// Statements vs Expressions
+
+// Expressions evaluate to a VALUE.
+
+42; // Numeric literal
+("hello"); // String literal
+true; // Boolean literal
+
+// Statements perform ACTIONS.
+// Statements control the flow of execution, perform operations, and structure the program.
+
+if (a > b) {
+  console.log("a is greater than b");
+} else if (a < b) {
+  console.log("a is less than b");
+} else {
+  console.log("a is equal to b");
+}
+
+// Example only {Expressions} can be placed when using template literals
+console.log(`I'm ${2024 - 1984} years old!`);
