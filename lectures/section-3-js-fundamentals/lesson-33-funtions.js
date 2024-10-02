ea9ea226dbb680bb51d2bc98156405c664345596
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-10-01 19:11:52 -0600

    adds notes on lesson 33 about functions

---
+++ lectures/section-3-js-fundamentals/lesson-33-funtions.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function logger() {
+  console.log(`My name is Sonny!`);
+}
+
+//Evoking the function
+logger();
+logger();
+logger();
+
+// Function with two parameters
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
